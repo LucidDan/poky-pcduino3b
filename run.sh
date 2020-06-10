7332bf68252876214688d471ddc3a9f713f58001
Dan Sloan <dan@luciddan.com>
2020-06-10 10:59:52 +1000

    Make run.sh proper script, and note that it currently doesn't work

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-
-docker run --rm -it --mount type=bind,source=$(pwd),target=/workdir --mount type=bind,source=$(pwd)/deploy,target=/worktmp/deploy -v yoctotmp:/worktmp crops/poky:ubuntu-18.04 --workdir=/workdir --id ${UID}:${GID}
+#!/bin/sh
+# this doesn't actually work, you need to hand-write the UID/GID at the moment
+# low priority, will fix eventually
+docker run --rm -it --mount type=bind,source=$(pwd),target=/workdir --mount type=bind,source=$(pwd)/deploy,target=/worktmp/deploy -v yoctotmp:/worktmp crops/poky:ubuntu-18.04 --workdir=/workdir --id=${UID}:${GID}
 
