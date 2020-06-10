caf66b84d50ca420352490a9725614376c7277ae
Dan Sloan <dan@luciddan.com>
2020-06-10 11:22:34 +1000

    Add/update license headers

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ pcduino
 A poky build env to hopefully build a functional image for the
 LinkSprite pcDuino 3b board, based on the Allwinner A20 cpu.
 
+This project is licensed under the Mozilla Public License (2.0).
+See LICENSE.md
--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+
+# Copyright (c) 2020 Daniel Sloan, Lucid Horizons Pty Ltd
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 # this doesn't actually work, you need to hand-write the UID/GID at the moment
 # low priority, will fix eventually
 docker run --rm -it --mount type=bind,source=$(pwd),target=/workdir --mount type=bind,source=$(pwd)/deploy,target=/worktmp/deploy -v yoctotmp:/worktmp crops/poky:ubuntu-18.04 --workdir=/workdir --id=${UID}:${GID}
